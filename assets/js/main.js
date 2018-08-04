d18d93642be0a21a79bd90ce022cda336a5f0646
Jake P <93plaistj@protonmail.ch>
2018-08-03 20:30:00 -0400

    CORS erorr / Sanitization / Second search FIXED

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,7 @@
-$(document).ready(function() {
-//=============================================================
+$(document).ready(function () {
 
-//              Notes for lyric API
-
-//=============================================================
-
-    // // Grab artist name from what is clicked (not an input)
-    // let artistName2 = "watsky";
-
-    // // Grab song name same way
-    // let songName2 = "Tiny Glowing Screens, Pt. 3";
-
-    // // Allows spaces eg. ".../coldplay/adventure of a life time", %20 workds aswell, NEEDS SPACES (toLowerCase)
-    // let queryURL2 = `https://api.lyrics.ovh/v1/${artistName2}/${songName2}`;
-
-    // $.ajax({
-    //     url: queryURL2,
-    //     method: "GET"
-    // }).then(function (response2) {
-    //     console.log(response2);
-    //     $(".displayPar").text(response2.lyrics);
-    // });
-
-//=============================================================
-
-// Ctrl + F to find "Temporary", These will be things that still need changed to work with final product
-
-//=============================================================
-    const database_config = () => {
-        //configuring database
+    //const database_config = () => {
+    //configuring database
         var config = {
             apiKey: "AIzaSyCCuhVM3rHwb0Qq8qrPlFWdCaQCEg0QYm0",
             authDomain: "band-aggregator.firebaseapp.com",
@@ -36,23 +9,54 @@ $(document).ready(function() {
             projectId: "band-aggregator",
             storageBucket: "band-aggregator.appspot.com",
             messagingSenderId: "432642484449"
-          };
-          firebase.initializeApp(config);
-          let bandDB = firebase.database();
-          //scrubs the search boxes input for database
-          let $searchBox = $('#searchBox').val().trim();
+        };
+        firebase.initializeApp(config);
+        let bandDB = firebase.database();
+    //scrubs the search boxes input for database
+    // let $searchBox = $('#searchBox').val().trim();
+    let artistCount = 0;
 
-          let artistInfo = {
-              artistName: $searchBox,
-              dateAdded: firebase.database.ServerValue.TIMESTAMP
-          }
-          bandDB.ref('artist_info').push($searchBox);
-    }
+    //let albumInfo = albumArray;
+    /*let artistInfo = {
+        artistName: $searchBox,
+        count: artistCount,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    }*/
+    //pushes input of search to database.
+
+
+    /*let ref = firebase.database().ref('Artists/artistName');
+    ref.once("value")
+        .then(function (snapshot) {
+            let snapA = snapshot.exists();
+            let snapB = snapshot.child("artistName" + $searchBox).exists();
+
+            if ($searchBox.snapshot === snapB) {
+                artistCount++;
+            }
+        })*/
+    //}
+    //=============================================================
+
+    // Ctrl + F to find "Temporary", These will be things that still need changed to work with final product
+
+    //=============================================================
+
+    //              Functions
+
+    //=============================================================
+    // database_config();
     // When an artists is searched
+    
     const itunesAlbumAJAX = () => {
-        database_config();       
+        //database_config();
+        let artistInput = $("#searchBox").val().trim();
         event.preventDefault();
-        
+        let artistInfo = {
+            artistName: artistInput,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        }
+        bandDB.ref('Artists').push(artistInfo);
 
         // Temporary
         // Static Div on HTML
@@ -60,47 +64,56 @@ $(document).ready(function() {
 
         // Temporary
         // Empties the display
+
         $tempDiv.empty();
 
         // Grabs the value of the search
-        let artistInput = $("#searchBox").val().trim();
+
+
 
         // Query URL for album search, artistInput only var interaction
-        let albumQueryURL = `https://itunes.apple.com/search?media=music&limit=15&entity=album&term=${artistInput}`
+        let albumQueryURL = `https://cors-anywhere.herokuapp.com/itunes.apple.com/search?media=music&limit=15&entity=album&term=${artistInput}`
 
         $.ajax({
             url: albumQueryURL,
             method: "GET",
-            datatype: "jsonp",
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
-            }
-        }).then(function(albumResponse) {
+            datatype: "json",
+        }).then(function (albumResponse) {
             // Parsing the response to make it a JSON object
             let parsedAlbumResponse = JSON.parse(albumResponse);
 
             // Shorthand for navigating the object
             let albumResults = parsedAlbumResponse.results;
+            let albumArray = [];
 
             // Loops over the results
-            $.each(albumResults, function(index, value) {
+            $.each(albumResults, function (index, value) {
+
+
+
                 // Temporary
                 // Created elements needed for interacting with the HTML
                 const $albumNamePar = $("<p>");
+                const $albumPictureImg = $("<img>");
                 const $albumNameDiv = $("<div>");
                 const $SongDiv = $("<div>");
                 const $fullGroupDiv = $("<div>");
 
                 // If the track count isn't one append album (prevents singles from being appended)
-                if(value.trackCount !== 1) {
+                // and if the track is in a holding array it wont trigger, (prevents duplicates)
+                if (value.trackCount !== 1 && $.inArray(value.collectionCensoredName, albumArray) === -1) {
+                    // Pushes the album name to a holding array
+                    albumArray.push(value.collectionCensoredName);
+
                     // Changes the text to the censored name
                     $albumNamePar.text(value.collectionCensoredName);
 
+                    // Adds and Image, can be either 60 or 100
+                    $albumPictureImg.attr("src", value.artworkUrl100);
+
                     // Adds attributes for[album-name, album,length, artist-name, album-index],
                     //  adds class of albumDiv (used for on click), appends $albumNamePar
-                    $albumNameDiv.attr("data-album-name", value.collectionCensoredName).attr("data-album-length", value.trackCount).attr("data-artist-name", value.artistName).attr("data-index", index).addClass("albumDiv").append($albumNamePar);
+                    $albumNameDiv.attr("data-album-name", value.collectionCensoredName).attr("data-album-length", value.trackCount).attr("data-artist-name", value.artistName).attr("data-index", index).addClass("albumDiv").append($albumPictureImg, $albumNamePar);
 
                     // Adds class of albumx to song div, empty div for storing songs eg. album4
                     $SongDiv.addClass("album" + index);
@@ -110,46 +123,46 @@ $(document).ready(function() {
 
                     // Appends the full group div to the display
                     $tempDiv.append($fullGroupDiv);
-                }                
+
+
+                    //sanatizes the search box after click
+                    $('#searchBox').val('');
+                }
             })
         })
     }
 
+
     // Temporary
     // Could not get fat arrow functions to interact with "this", if addressed refactor with ES6
-        // const itunesSongAJAX = () => {
+    // const itunesSongAJAX = () => {
 
-        //     let $this = $(this);
-        //     console.log($this);
+    //     let $this = $(this);
+    //     console.log($this);
 
-        //     let albumName = $this.attr("data-album-name");
-        //     console.log(albumName);
-            
-        //     let songQueryURL = `https://itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${trackCount}`
-        // }
+    //     let albumName = $this.attr("data-album-name");
+    //     console.log(albumName);
+
+    //     let songQueryURL = `https://itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${trackCount}`
+    // }
 
     // Called when album is clicked
     function TEMPitunesSongAJAX() {
         // Shorthand
-        let $this = $(this);
-        let albumName = $this.attr("data-album-name");
-        let albumLength = $this.attr("data-album-length");
-        let artistName = $this.attr("data-artist-name");
-        let $albumIndex = $(".album" + $this.attr("data-index"));
+        let $thisAlbum = $(this);
+        let albumName = $thisAlbum.attr("data-album-name");
+        let albumLength = $thisAlbum.attr("data-album-length");
+        let albumArtistName = $thisAlbum.attr("data-artist-name");
+        let $albumIndex = $(".album" + $thisAlbum.attr("data-index"));
 
         // Query for Song search, limits to album length
-        let songQueryURL = `https://itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${albumLength}`;
+        let songQueryURL = `https://cors-anywhere.herokuapp.com/itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${albumLength}`;
 
         $.ajax({
             url: songQueryURL,
             method: "GET",
-            datatype: "jsonp",
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
-            }
-        }).then(function(songResponse) {
+            datatype: "json"
+        }).then(function (songResponse) {
             // Parsing the response to make it a JSON object
             let parsedSongResponse = JSON.parse(songResponse);
 
@@ -157,16 +170,16 @@ $(document).ready(function() {
             let songResults = parsedSongResponse.results;
 
             // If the data-state is open it empties the song div
-            if($this.attr("data-state") === "open") {
+            if ($thisAlbum.attr("data-state") === "open") {
                 // Sets data-state to closed
-                $this.attr("data-state", "closed");
+                $thisAlbum.attr("data-state", "closed");
                 $albumIndex.empty();
             }
 
             // If the data-state is not open, opens it            
             else {
                 // Loop for songs
-                $.each(songResults, function(index, value) {
+                $.each(songResults, function (index, value) {
                     // Shorthand
                     const $songNamePar = $("<p>");
                     const $songNameDiv = $("<div>");
@@ -175,27 +188,49 @@ $(document).ready(function() {
                     $songNamePar.text(value.trackCensoredName);
 
                     // Adds attributes for[song-name, artist-name], adds class of songDiv, appends songNamePar
-                    $songNameDiv.attr("data-song-name", value.trackCensoredName).attr("data-artist-name", artistName).addClass("songDiv").append($songNamePar);
+                    $songNameDiv.attr("data-song-name", value.trackCensoredName).attr("data-artist-name", albumArtistName).addClass("songDiv").append($songNamePar);
 
                     // Appends the Song name div to the empty song div
                     $albumIndex.append($songNameDiv);
-                    
+
                     // Sets the attribute of data-state to open
-                    $this.attr("data-state", "open")
+                    $thisAlbum.attr("data-state", "open")
                 });
             }
         });
     }
 
-//=============================================================
+    function TEMPlyricsAJAX() {
+        let $thisSong = $(this);
+        let songArtistName = $thisSong.attr("data-artist-name");
+        let songName = $thisSong.attr("data-song-name");
+        const $lyricsDiv = $(".tempLyricsDiv");
+        $lyricsDiv.addClass("line-break lyricsDiv").empty();
 
-//              On Clicks
 
-//=============================================================
+        // // Allows spaces eg. ".../coldplay/adventure of a life time", %20 workds aswell, NEEDS SPACES (toLowerCase)
+        let queryURL2 = `https://api.lyrics.ovh/v1/${songArtistName}/${songName}`;
+
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function (lyricsResponse) {
+            console.log(lyricsResponse);
+            $lyricsDiv.text(lyricsResponse.lyrics)
+        });
+    }
+
+    //=============================================================
+
+    //              On Clicks
+
+    //=============================================================
 
     $("#searchBtn").on("click", itunesAlbumAJAX);
     $(".tempDiv").on("click", ".albumDiv", TEMPitunesSongAJAX);
-  //  $(".tempDiv").on("click", ".songDiv", TEMPlyricsAJAX)
+    $(".tempDiv").on("click", ".songDiv", TEMPlyricsAJAX);
 
-//=============================================================
+    //database_config();
+    //=============================================================
+
 });

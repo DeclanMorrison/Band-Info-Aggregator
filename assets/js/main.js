a52affc0184cf2c4dbb578b6de7d265881acb32d
Jake P <93plaistj@protonmail.ch>
2018-08-05 21:39:56 -0400

    Top 5 / recent -> refactor bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,100 @@
-$(document).ready(function() {
-//=============================================================
+$(document).ready(function () {
+    //=============================================================
 
-// Ctrl + F to find "Temporary", These will be things that still need changed to work with final product
+    // Ctrl + F to find "Temporary", These will be things that still need changed to work with final product
 
-//=============================================================
+    //=============================================================
 
-//              FireBase
+    //              FireBase
 
-//=============================================================
-//Note
-//Firebase stuff
-// bandDB.ref('Artists').on("value", function(snapshot){
-//     $('#recents').empty();
-//        console.log('here');
-//        let mostRecent = snapshot.val().name;
-//        console.log(mostRecent);
-//        JSON.stringify(mostRecent);
-//        let $recentDiv = $("#recents");
-//        let $P = $('<p>').text('Most Recent: ' + mostRecent);
+    //=============================================================
+    //Note
+    //Firebase stuff
+    // bandDB.ref('Artists').on("value", function(snapshot){
+    //     $('#recents').empty();
+    //        console.log('here');
+    //        let mostRecent = snapshot.val().name;
+    //        console.log(mostRecent);
+    //        JSON.stringify(mostRecent);
+    //        let $recentDiv = $("#recents");
+    //        let $P = $('<p>').text('Most Recent: ' + mostRecent);
 
-//        $recentDiv.append($P);
-//        $('#recents').append($recentDiv);
-//       // $($P).empty();
-//    });
-    
-//=============================================================
+    //        $recentDiv.append($P);
+    //        $('#recents').append($recentDiv);
+    //       // $($P).empty();
+    //    });
 
-//              Functions
 
-//=============================================================
+    var config = {
+        apiKey: "AIzaSyCCuhVM3rHwb0Qq8qrPlFWdCaQCEg0QYm0",
+        authDomain: "band-aggregator.firebaseapp.com",
+        databaseURL: "https://band-aggregator.firebaseio.com",
+        projectId: "band-aggregator",
+        storageBucket: "band-aggregator.appspot.com",
+        messagingSenderId: "432642484449"
+    };
+    firebase.initializeApp(config);
+    let bandDB = firebase.database();
+
+
+    $('#top').hide();
+    //loops through entirity of database checks records
+    bandDB.ref('searches/').orderByChild('searchCount')
+        .limitToLast(5).on('value',  (snapshot) => { //limits the amound appended to 5. 
+            let topFive = '<ul>'
+            snapshot.forEach((childSnapshot) => {
+                console.log(childSnapshot.val());
+                let childRecord = childSnapshot.val(); //sets variable of childSnapshot
+                topFive += '<li>' + childRecord.artist + ' Searches = ' +
+                    childRecord.searchCount + '</li>'
+            });
+            topFive += '</ul>';
+            $('#top').html(topFive);
+        });
+
+    $('#search-btn').on('click', () => {
+        event.preventDefault();
+        $('#top').show();
+        let searchTerm = $('#band-name').val().toLowerCase().trim();
+        var searchCount = 0;
+
+        bandDB.ref('searches/' + searchTerm).on('value',  (snapshot) => {
+            searchCount = (snapshot.val() && snapshot.val().searchCount) || 0;
+            //console.log('snap' + snapshot);
+        });
+
+        searchCount = searchCount + 1;
+
+        bandDB.ref('searches/' + searchTerm).set({
+            searchCount: searchCount,
+            artist: searchTerm
+        }, (error)=> {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('data saved madude');
+            }
+        });
+    })
+        //to show what the current snapshot is
+    let searchRef = firebase.database().ref('searches/');
+    searchRef.on('value', (snapshot) => {
+        console.log(snapshot);
+    });
+
+
+    //=============================================================
+
+    //              Functions
+
+    //=============================================================
 
     // When an artists is searched
     function itunesAlbumAJAX() {
         // Prevents the page from refreshing on submit
-        event.preventDefault();
-
-        //Note
-        //firebase stuff
-        // bandDB.ref('Artists').set({
-        //     name: artistInput
-        // });
 
         // Holding variables for an Index and an array used to prevent duplicate albums
-        let albumOnIndex = 0;        
+        let albumOnIndex = 0;
         let albumArray = [];
 
         $(".collapsible-body").attr("style", "");
@@ -52,14 +104,16 @@ $(document).ready(function() {
         let artistInput = $("#band-name").val().trim();
 
         // Query URL for album search, artistInput only var interaction
-        let albumQueryURL = `https://itunes.apple.com/search?media=music&limit=15&entity=album&term=${artistInput}`
+        //`https://cors-anywhere.herokuapp.com/ -> add to front of queryURL to get running on live
+        let albumQueryURL = `https://cors-anywhere.herokuapp.com/itunes.apple.com/search?media=music&limit=15&entity=album&term=${artistInput}`
 
         // Call for getting the album info
         $.ajax({
             url: albumQueryURL,
             method: "GET",
-            datatype: "json",            
-        }).then(function(albumResponse) {
+            datatype: "json",
+        }).then(function (albumResponse) {
+            console.log('call went through');
             // Parsing the response to make it a JSON object
             let parsedAlbumResponse = JSON.parse(albumResponse);
 
@@ -71,19 +125,22 @@ $(document).ready(function() {
             $(".card-title").text(albumResults[0].artistName)
 
             // Loops over the results
-            $.each(albumResults, function(index, value) {
+            $.each(albumResults, function (index, value) {
                 // If the track count is bigger than one (prevents singles on first scan),
                 // the album isnt in the holding array (prevents duplicates),
                 // and the albumOnIndex is less than 5 (stops search at 5 results)
-                if(value.trackCount !== 1 && $.inArray(value.collectionCensoredName, albumArray) === -1 && albumOnIndex < 5) {
+                if (value.trackCount !== 1 &&
+                    $.inArray(value.collectionCensoredName, albumArray) === -1 && albumOnIndex < 5) {
                     // Pushes the album name to a holding array
-                    albumArray.push(value.collectionCensoredName);                    
+                    albumArray.push(value.collectionCensoredName);
 
                     // Changes the text of the album div we are on to the name
                     $(".album" + albumOnIndex + "Name").text(value.collectionCensoredName);
 
                     // Adds attributes for[album-name, album-length, album-index],
-                    $(".album" + albumOnIndex + "Div").attr("data-album-name", value.collectionCensoredName).attr("data-album-length", value.trackCount).attr("data-index", albumOnIndex);
+                    $(".album" + albumOnIndex + "Div")
+                        .attr("data-album-name", value.collectionCensoredName)
+                        .attr("data-album-length", value.trackCount).attr("data-index", albumOnIndex);
 
                     // Changes the src of the img to reflect the album name
                     $(".album" + albumOnIndex + "Img").attr("src", value.artworkUrl100);
@@ -94,20 +151,21 @@ $(document).ready(function() {
             })
 
             // If weve added less than 5 albums (no need to re-scan if we already have 5)            
-            if(albumOnIndex < 5) {
+            if (albumOnIndex < 5) {
                 // Loops over the result
-                $.each(albumResults, function(index, value) {
+                $.each(albumResults, function (index, value) {
                     // If the album isnt in the holding array (prevents duplicates),
                     // and the albumOnIndex is less than 5 (stops search at 5 results)
-                    if($.inArray(value.collectionCensoredName, albumArray) === -1 && albumOnIndex < 5) {
+                    if ($.inArray(value.collectionCensoredName, albumArray) === -1 && albumOnIndex < 5) {
                         // Pushes the album name to a holding array
-                        albumArray.push(value.collectionCensoredName);                    
-    
+                        albumArray.push(value.collectionCensoredName);
+
                         // Changes the text to the censored name
                         $(".album" + albumOnIndex + "Name").text(value.collectionCensoredName);
-    
+
                         // Adds attributes for[album-name, album-length, album-index]
-                        $(".album" + albumOnIndex + "Div").attr("data-album-name", value.collectionCensoredName).attr("data-album-length", value.trackCount).attr("data-index", albumOnIndex);
+                        $(".album" + albumOnIndex + "Div").attr("data-album-name", value.collectionCensoredName)
+                            .attr("data-album-length", value.trackCount).attr("data-index", albumOnIndex);
 
                         // Changes the src of the img to reflect the album name    
                         $(".album" + albumOnIndex + "Img").attr("src", value.artworkUrl100);
@@ -116,8 +174,49 @@ $(document).ready(function() {
                         albumOnIndex++;
                     }
                 })
-            }            
-        }) 
+            }
+            $('#band-name').val(''); //last fix
+
+            let artistInfo = { //structure of how all inputs will be added
+                artistName: {
+                    albums: albumArray,
+                    name: artistInput,
+                },
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            }
+            //
+            bandDB.ref('Artists').on("value", (snapshot) => {
+                $('#recents').empty();
+                console.log('here');
+                let mostRecent = snapshot.val().name;
+                // console.log(mostRecent);
+                JSON.stringify(mostRecent);  //changes output from object to str
+                let $recentDiv = $("#recents");
+                let $P = $('<p>').text('Most Recent: ' + mostRecent);
+                $recentDiv.append($P);
+                $('#recents').append($recentDiv);
+            });
+            bandDB.ref('Artists').set({
+                name: artistInput
+            });
+            //references 'all-Artists' then orders by key, and limits to the last 2 objects in group
+            let keyQuery = firebase.database().ref('all-Artists').limitToLast(2); 
+            keyQuery.once("value")
+                .then((snapshot) => {
+                    let snapShot = snapshot.val();
+                    //snapShot.val();
+                    JSON.stringify(snapShot);
+                    console.log(snapShot);
+                    console.log('here');
+                       // console.log(previousSnap.val());
+                    //  let lowestKey = previousSnap.getChildren().val();
+                     // console.log(lowestKey);
+                   
+                })    
+        
+            bandDB.ref('all-Artists').push(artistInfo); 
+        })
+        
     }
 
     // Called when album is clicked
@@ -127,19 +226,20 @@ $(document).ready(function() {
         let albumName = $thisAlbum.attr("data-album-name");
         let albumLength = $thisAlbum.attr("data-album-length");
         let forAlbumIndex = $thisAlbum.attr("data-index");
-        
+
         // Empties the Div that was clicked
         $(".song" + forAlbumIndex + "Div").empty();
 
         // Query for Song search, limits to album length
-        let songQueryURL = `https://itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${albumLength}`;
+        //`https://cors-anywhere.herokuapp.com/ -> add to front of queryURL to get running on live
+        let songQueryURL = `https://cors-anywhere.herokuapp.com/itunes.apple.com/search?media=music&entity=song&term=${albumName}&limit=${albumLength}`;
 
         // Call for getting the song info
         $.ajax({
             url: songQueryURL,
             method: "GET",
             datatype: "json"
-        }).then(function(songResponse) {
+        }).then(function (songResponse) {
             // Parsing the response to make it a JSON object
             let parsedSongResponse = JSON.parse(songResponse);
 
@@ -152,20 +252,22 @@ $(document).ready(function() {
             $songCollection.addClass("collection");
 
             // Loop for songs
-            $.each(songResults, function(index, value) {
+            $.each(songResults, function (index, value) {
                 // Adds content to manipulate for the collection
                 const $songCollectionItem = $("<li>");
                 $songCollectionItem.addClass("collection-item");
 
                 // Changes the Text to the song name
-                $songCollectionItem.text(value.trackCensoredName).attr("data-song-name", value.trackCensoredName).attr("data-artist-name", value.artistName);
+                $songCollectionItem.text(value.trackCensoredName)
+                    .attr("data-song-name", value.trackCensoredName)
+                    .attr("data-artist-name", value.artistName);
 
                 // Appends the data for the collection to the collection
                 $($songCollection).append($songCollectionItem);
             });
-            
+
             // Appends the collection to the div (which shows when an album is clicked) under the albume
-            $(".song" + forAlbumIndex + "Div").append($songCollection);            
+            $(".song" + forAlbumIndex + "Div").append($songCollection);
         });
     }
 
@@ -180,7 +282,7 @@ $(document).ready(function() {
         const $lyricsPar = $(".lyricsPar");
 
         $lyricsPar.empty();
-        
+
         let queryURL2 = `https://api.lyrics.ovh/v1/${songArtistName}/${songName}`;
 
         $.ajax({
@@ -194,56 +296,56 @@ $(document).ready(function() {
         $lyricsModal.modal("open");
     }
 
-    $('#band-name').keyup(function (event){
-        if (event.which === 13) {
+    $('#search-btn').on("keyup keypress", function (event) {
+        let keyCode = event.keyCode || event.which;
+        if (keyCode === 13) {
             itunesAlbumAJAX();
             // wikiAJAXcall();
-
             event.preventDefault();
-            
+
             $(".title").addClass("min");
 
             $(".band-image").addClass("fadeInLeftBig").removeClass("hide");
-    
-            setTimeout(function(){
+
+            setTimeout(function () {
                 $(".bio").addClass("fadeInRightBig").removeClass("hide");
-                setTimeout(function(){
+                setTimeout(function () {
                     $(".collapsible").addClass("fadeInUpBig").removeClass("hide");
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $(".footer-copyright").addClass("fadeInUpBig").removeClass("hide");
-                    },250)
-                },250)
+                    }, 250)
+                }, 250)
             }, 250)
-            
+
             return false;
         }
     });
 
-//=============================================================
+    //=============================================================
 
-//              On Clicks
+    //              On Clicks
 
-//=============================================================
+    //=============================================================
 
-    $("#search-btn").on("click", function (){
+    $("#search-btn").on("click", function () {
         itunesAlbumAJAX();
         // wikiAJAXcall();
         $(".title").addClass("min");
 
         $(".band-image").addClass("fadeInLeftBig").removeClass("hide");
 
-        setTimeout(function(){
+        setTimeout(function () {
             $(".bio").addClass("fadeInRightBig").removeClass("hide");
-            setTimeout(function(){
+            setTimeout(function () {
                 $(".collapsible").addClass("fadeInUpBig").removeClass("hide");
-                setTimeout(function(){
+                setTimeout(function () {
                     $(".footer-copyright").addClass("fadeInUpBig").removeClass("hide");
-                },250)
-            },250)
+                }, 250)
+            }, 250)
         }, 250)
     });
 
-    $('#band-name').each(function (){
+    $('#band-name').each(function () {
         const elem = $(this);
         // Look for changes in the value
         elem.bind("changes input paste", function (event) {
@@ -259,8 +361,8 @@ $(document).ready(function() {
     $(".collapsible-header").on("click", itunesSongAJAX);
     $(".album-list").on("click", ".collection-item", lyricsAJAX)
 
-//=============================================================
+    //=============================================================
 
     $(".collapsible").collapsible();
-    $(".modal").modal()  
+    $(".modal").modal()
 });
